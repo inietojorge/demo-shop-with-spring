b31b269240a59ef59d905183aac765ad2c5e61e7
Ignacio Nieto Jorge <ignacio.nietojorge@dxc.com>
2023-01-20 16:51:19 +0100

    Remove unneeded Service annotation from interface

--- src/main/java/com/luxoft/demoshopwithspring/service/ProductService.java
+++ src/main/java/com/luxoft/demoshopwithspring/service/ProductService.java
@@ -2,11 +2,10 @@ package com.luxoft.demoshopwithspring.service;
 
 import com.luxoft.demoshopwithspring.domain.ProductDto;
 import com.luxoft.demoshopwithspring.exception.ShopException;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
+
 public interface ProductService {
     ProductDto addProductToInventory(ProductDto productDto);
     ProductDto removeProductFromInventory(ProductDto productDto) throws ShopException;
